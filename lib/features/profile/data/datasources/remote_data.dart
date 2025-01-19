81ee70baf3d4b654e1e23c040a1f61cf79867e99
nenz24 <danendracool08@gmail.com>
2025-01-19 17:14:45 +0800

    updating

--- lib/features/profile/data/datasources/remote_data.dart
+++ lib/features/profile/data/datasources/remote_data.dart
@@ -1,8 +1,8 @@
-
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 
+import '../../../../core/error/exception.dart';
 import '../models/model_profile.dart';
 
 abstract class ProfileRemoteDataSource {
@@ -10,25 +10,40 @@ abstract class ProfileRemoteDataSource {
   Future<ProfileModel> getUser(int id);
 }
 
-class ProfileRemoteDataSourceImplementation extends ProfileRemoteDataSource{
+class ProfileRemoteDataSourceImplementation extends ProfileRemoteDataSource {
+  final http.Client client;
+
+  ProfileRemoteDataSourceImplementation({
+    required this.client,
+  });
+
   @override
   Future<List<ProfileModel>> getAllUser(int page) async {
     Uri url = Uri.parse('https://reqres.in/api/users?page=$page');
     var response = await http.get(url);
-
-    Map<String, dynamic> dataBody = jsonDecode(response.body);
-    List<dynamic> data = dataBody['data'];
-    return ProfileModel.fromJsonList(data);
+    if (response.statusCode == 200) {
+      Map<String, dynamic> dataBody = jsonDecode(response.body);
+      List<dynamic> data = dataBody['data'];
+      return ProfileModel.fromJsonList(data);
+    } else if (response.statusCode == 404) {
+      throw EmptyException(message: 'Data not found - error 404');
+    } else {
+      throw GeneralException(message: 'cannot get data');
+    }
   }
 
   @override
   Future<ProfileModel> getUser(int id) async {
     Uri url = Uri.parse('https://reqres.in/api/users/$id');
-    var response =  await http.get(url);
-
-    Map<String, dynamic> dataBody = jsonDecode(response.body);
-    Map<String, dynamic> data = dataBody['data'];
-    return ProfileModel.fromJson(data);
+    var response = await http.get(url);
+    if (response.statusCode == 200) {
+      Map<String, dynamic> dataBody = jsonDecode(response.body);
+      Map<String, dynamic> data = dataBody['data'];
+      return ProfileModel.fromJson(data);
+    } else if (response.statusCode == 404) {
+      throw EmptyException(message: 'Data not found - error 404');
+    } else {
+      throw GeneralException(message: 'cannot get data');
+    }
   }
-
 }
---
+++ lib/features/profile/data/repositories/profile_repository_implementation.dart
@@ -0,0 +1,66 @@
+import 'package:dartz/dartz.dart';
+import 'package:hive/hive.dart';
+import 'package:water_tracker/core/error/failure.dart';
+import 'package:water_tracker/features/profile/data/datasources/local_data.dart';
+import 'package:water_tracker/features/profile/data/datasources/remote_data.dart';
+import 'package:water_tracker/features/profile/data/models/model_profile.dart';
+import 'package:water_tracker/features/profile/domain/entities/data_profile.dart';
+import 'package:water_tracker/features/profile/domain/repositories/repo_profile.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
+
+class ProfileRepositoryImplementation extends ProfileRepository {
+  final ProfileRemoteDataSource profileRemoteDataSource;
+  final ProfileLocalDataSource profileLocalDataSource;
+  final HiveInterface hive;
+
+  ProfileRepositoryImplementation(
+      {required this.profileLocalDataSource,
+      required this.profileRemoteDataSource,
+      required this.hive});
+
+  @override
+  Future<Either<Failure, List<Profile>>> getAllUser(int page) async {
+    try {
+      final List<ConnectivityResult> connectivityResult =
+          await (Connectivity().checkConnectivity());
+
+      if (connectivityResult.contains(ConnectivityResult.none)) {
+        // No available network types
+        List<ProfileModel> hasil =
+            await profileLocalDataSource.getAllUser(page);
+        return Right(hasil);
+      } else {
+        // available network
+        List<ProfileModel> hasil =
+            await profileRemoteDataSource.getAllUser(page);
+        var box = hive.box('profile_box');
+        box.put('getAllUser', hasil);
+        return Right(hasil);
+      }
+    } catch (e) {
+      return Left(Failure());
+    }
+  }
+
+  @override
+  Future<Either<Failure, Profile>> getUser(int id) async {
+    try {
+      final List<ConnectivityResult> connectivityResult =
+          await (Connectivity().checkConnectivity());
+
+      if (connectivityResult.contains(ConnectivityResult.none)) {
+        // No available network types
+        ProfileModel hasil = await profileLocalDataSource.getUser(id);
+        return Right(hasil);
+      } else {
+        // available network
+        ProfileModel hasil = await profileRemoteDataSource.getUser(id);
+        var box = hive.box('profile_box');
+        box.put('getUser', hasil);
+        return Right(hasil);
+      }
+    } catch (e) {
+      return Left(Failure());
+    }
+  }
+}
